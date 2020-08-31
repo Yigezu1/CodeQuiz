be4ece1ca9f502a0fd055f57b2bfdb3c9e7af3f6
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-08-30 20:22:51 -0400

    index and asset files added to the project

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var timeLeft = document.querySelector(".timeleft");
+var options = document.querySelector(".options");
+var answeralert = document.querySelector(".answeralert");
+var startBtn = document.querySelector(".startbutton");
+
+var questions = [{
+    quo: "",
+    opt: [option1, option2, option3, option4],
+    ans: this.opt[0]
+},
+{
+    quo: "",
+    opt: [option1, option2, option3, option4],
+    ans: this.opt[0]
+},
+{
+    quo: "",
+    opt: [option1, option2, option3, option4],
+    ans: this.opt[0]
+},
+{
+    quo: "",
+    opt: [option1, option2, option3, option4],
+    ans: this.opt[0]
+}
+];
+
+function questionsHandler(e){
+    
+    var li = document.createElement("li");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+startBtn.addEventListener("click",questionsHandler);
