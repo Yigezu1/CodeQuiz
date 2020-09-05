2c24302f68c4cb22bc8daa21b3497eb241a56475
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-09-05 14:10:08 -0400

    Updated index and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,33 @@ var questionPEl = document.querySelector("#question");
 var questions = [
   {
     quo: "This is the first question",
-    opt: ["option1", "option2", "option3", "option4"],
-    
+    opt: ["option1", "option2", "option3", "option4"],    
   },
   {
-    quo: "",
+    quo: "This is the second question",
     opt: ["option1", "option2", "option3", "option4"],
    
   },
   {
-    quo: "",
+    quo: "This is the third question",
     opt: ["option1", "option2", "option3", "option4"],
     
   },
   {
-    quo: "",
+    quo: "This is the forth question",
     opt: ["option1", "option2", "option3", "option4"],
     
   },
+  {
+    quo: "This is the fifth question",
+    opt: ["option1", "option2", "option3", "option4"],
+    
+  }
 ];
-
+var correctAnswer =[];
 function timeInterval(){
-    var tim =70;
-    timeLeft.textContent = 70 + "s";
+    var tim =75;
+    timeLeft.textContent = 75 + "s";
     setInterval(function(){
         if(tim > 0){
         tim--;
